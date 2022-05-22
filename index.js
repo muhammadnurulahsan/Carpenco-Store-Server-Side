9c6ae3e4e6c7849c44c14b0bc7a49afbfacbe1a3
muhammadnurulahsan <mdnurulahsanbdbd@gmail.com>
2022-05-22 20:27:31 +0600

    heroku added

--- index.js
+++ index.js
@@ -8,6 +8,18 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://carpenco_store:<password>@cluster0.9fomd.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
+
 //Root Routes
 app.get("/", (req, res) => {
   res.send("Backend Server IS Running ");
