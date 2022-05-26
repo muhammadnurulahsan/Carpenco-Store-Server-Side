cfe5729fafddd78de9c82d77466eea71a915e8a2
muhammadnurulahsan <mdnurulahsanbdbd@gmail.com>
2022-05-26 16:26:03 +0600

    Check User Api Added

--- index.js
+++ index.js
@@ -249,14 +249,6 @@ async function run() {
       res.send(result);
     });
 
-    // CHECK A USER IS ADMIN OR NOT
-    app.get("/admin/:email", async (req, res) => {
-      const email = req.params.email;
-      const user = await usersCollection.findOne({ email: email });
-      const isAdmin = user?.role === "admin";
-      res.send({ admin: isAdmin });
-    });
-
     // GET ALL ORDERS
     app.get("/orders", verifyToken, async (req, res) => {
       if (req.query.email) {
@@ -268,6 +260,14 @@ async function run() {
         const result = await ordersCollection.find({}).toArray();
         res.send(result);
       }
+    });
+
+    // CHECK A USER IS ADMIN OR NOT
+    app.get("/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = await usersCollection.findOne({ email: email });
+      const isAdmin = user?.role === "admin";
+      res.send({ admin: isAdmin });
     });
 
     // GET ORDERS BASED ON ID
